d7d5507620448832d9f706f3d84a78cfed65b802
premsai7744 <premsai7744@gmail.com>
2026-06-23 10:51:36 +0530

    Initial commit

---
+++ tech-specs-services/src/main/java/com/premit/tech_specs_services/TechSpecsServicesApplication.java
@@ -0,0 +1,13 @@
+package com.premit.tech_specs_services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechSpecsServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechSpecsServicesApplication.class, args);
+	}
+
+}
---
+++ tech-specs-services/src/test/java/com/premit/tech_specs_services/TechSpecsServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.premit.tech_specs_services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechSpecsServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
